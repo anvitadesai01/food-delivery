afe660239be2fd446a73dc10e0988a978432615c
Anvita Desai <anvitadesai@team.bigscal.com>
2026-04-02 11:08:49 +0530

    Revert "Implement bull in order placement API"
    This reverts commit 99ebed85454735eaebe3be43b1a602250bd56969.

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const orderQueue = require("../queues/order.queue");
 
 const Order = require("../models/order.model");
 const Cart = require("../models/cart.model");
@@ -103,16 +102,6 @@ const placeOrder = async (req, res, next) => {
 
     await session.commitTransaction();
     session.endSession();
-
-    await orderQueue.add(
-      "orderPlaced",
-      { orderId: order._id },
-      {
-        attempts: 2,
-        backoff: 3000,
-        removeOnComplete: true,
-      }
-    );
 
     return res
       .status(201)
